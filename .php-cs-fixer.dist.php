af089d4ff397cd5b61ab172bc80ec42a52c5ce2e
Damien Chauveau <damienchauveau@hotmail.fr>
2026-06-03 15:39:52 +0200

    ci: add code quality tooling (PHP CS Fixer + PHPStan + GitHub Actions)
    - Configure PHP CS Fixer with Symfony preset + PHP 8.4 migration rules
    - Configure PHPStan level 6 (will upgrade to level 8 after entities)
    - Add composer scripts: cs:check, cs:fix, stan, test, qa
    - Apply CS Fixer to existing files (added declare(strict_types=1))
    - Add GitHub Actions CI workflow with quality + tests jobs
    - Cache composer dependencies for faster CI runs

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,17 +1,42 @@
 <?php
 
-$finder = (new PhpCsFixer\Finder())
-    ->in(__DIR__)
-    ->exclude('var')
-    ->notPath([
-        'config/bundles.php',
-        'config/reference.php',
-    ])
-;
+declare(strict_types=1);
 
-return (new PhpCsFixer\Config())
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+
+$finder = Finder::create()
+    ->in([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ])
+    ->exclude([
+        'var',
+        'vendor',
+        'node_modules',
+    ]);
+
+return (new Config())
+    ->setRiskyAllowed(true)
     ->setRules([
         '@Symfony' => true,
+        '@Symfony:risky' => true,
+        '@PHP84Migration' => true,
+        '@PHP82Migration:risky' => true,
+        'declare_strict_types' => true,
+        'strict_param' => true,
+        'strict_comparison' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'no_unused_imports' => true,
+        'ordered_imports' => ['sort_algorithm' => 'alpha'],
+        'phpdoc_align' => ['align' => 'left'],
+        'phpdoc_order' => true,
+        'concat_space' => ['spacing' => 'one'],
+        'global_namespace_import' => [
+            'import_classes' => true,
+            'import_constants' => false,
+            'import_functions' => false,
+        ],
     ])
     ->setFinder($finder)
-;
+    ->setCacheFile(__DIR__ . '/var/.php-cs-fixer.cache');
